4575cf4f17ad757b5fb90d9a3b3f4c596503f780
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-07-10 07:48:40 +0900

    Change: Seperate Role

--- lib/discorb/guild.rb
+++ lib/discorb/guild.rb
@@ -7,6 +7,7 @@ require_relative 'color'
 require_relative 'member'
 require_relative 'channel'
 require_relative 'permission'
+require_relative 'role'
 
 module Discorb
   class SystemChannelFlag < Flag
@@ -154,87 +155,6 @@ module Discorb
       @max_presences = data[:max_presences]
       @stage_instances = nil # TODO: Array<Discorb::StageInstance>
       @_data.update(data)
-    end
-  end
-
-  class Role < DiscordModel
-    attr_reader :id, :name, :color, :permissions, :position
-
-    include Comparable
-    def initialize(client, guild, data)
-      @client = client
-      @guild = guild
-      _set_data(data)
-    end
-
-    def <=>(other)
-      @position <=> other.position
-    end
-
-    def to_s
-      "<@&#{@id}>"
-    end
-
-    def color?
-      @color != 0
-    end
-
-    def hoist?
-      @hoist
-    end
-
-    def managed?
-      @managed
-    end
-
-    def mentionable?
-      @mentionable
-    end
-
-    def update!
-      Async do
-        _resp, data = @client.internet.get("/guilds/#{@guild_id}/roles").wait
-        _set_data(data.find { |r| r[:id] == @id })
-      end
-    end
-
-    def inspect
-      "#<#{self.class} @#{@name} id=#{@id}>"
-    end
-
-    class Tag
-      attr_reader :bot_id, :integration_id, :premium_subscriber
-
-      def initialize(data)
-        @bot_id = Snowflake.new(data[:bot_id])
-        @integration_id = Snowflake.new(data[:bot_id])
-        @premium_subscriber = Snowflake.new(data[:bot_id])
-      end
-
-      def bot?
-        !@bot_id.nil?
-      end
-
-      def integration?
-        !@integration_id.nil?
-      end
-
-      def premium_subscriber?
-        !!@premium_subscriber
-      end
-    end
-
-    def _set_data(data)
-      @id = Snowflake.new(data[:id])
-      @name = data[:name]
-      @color = Color.new(data[:color])
-      @hoist = data[:hoist]
-      @position = data[:position]
-      @permissions = Permission.new(data[:permissions].to_i)
-      @managed = data[:managed]
-      @mentionable = data[:mentionable]
-      @tags = data[:tags] || {}
-      @guild.roles[@id] = self
     end
   end
 
