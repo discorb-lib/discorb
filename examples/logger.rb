60244aa45ead7ec509f31c17ff8ec84e0b7754ed
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-07-10 07:51:07 +0900

    Update: Update logger

--- examples/logger.rb
+++ examples/logger.rb
@@ -7,7 +7,11 @@ require_relative '../lib/discorb'
 client = Discorb::Client.new(log: $stdout, colorize_log: true)
 
 def get_log_channel(guild)
-  guild.channels.find { |c| c.name == 'channel-log' }
+  guild.channels.find { |c| c.name == 'log' }
+end
+
+client.on :ready do
+  puts "Logged in as #{client.user}"
 end
 
 client.on :channel_create do |_task, channel|
@@ -29,4 +33,25 @@ client.on :channel_delete do |_task, channel|
     'Channel Deleted', "`#{channel.name}`", color: Discorb::Color[:red], timestamp: Time.now
   )
 end
+
+client.on :role_create do |_task, role|
+  get_log_channel(role.guild).post embed: Discorb::Embed.new(
+    'Role Created', "Name: `#{role.name}`\n\n#{role}", color: Discorb::Color[:green], timestamp: Time.now
+  )
+end
+
+client.on :role_update do |_task, before, after|
+  next if before.name == after.name
+
+  get_log_channel(after.guild).post embed: Discorb::Embed.new(
+    'Role Renamed', "`#{before.name}` -> `#{after.name}`", color: Discorb::Color[:yellow], timestamp: Time.now
+  )
+end
+
+client.on :role_delete do |_task, role|
+  get_log_channel(role.guild).post embed: Discorb::Embed.new(
+    'Role Deleted', "`#{role.name}`", color: Discorb::Color[:red], timestamp: Time.now
+  )
+end
+
 client.run(ENV['discord_bot_token'])
