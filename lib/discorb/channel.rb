8ee60a94d34b9478a5a8b1aa2b32db3bd8c4e50d
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-07-21 06:29:38 +0900

    Add: Add reason to Channel#edit

--- lib/discorb/channel.rb
+++ lib/discorb/channel.rb
@@ -125,7 +125,7 @@ module Discorb
       @threads = []
     end
 
-    def edit(name: nil, announce: nil, position: nil, topic: nil, nsfw: nil, slowmode: nil, category: nil, parent: nil)
+    def edit(name: nil, announce: nil, position: nil, topic: nil, nsfw: nil, slowmode: nil, category: nil, parent: nil, reason: nil)
       Async do
         payload = {}
         payload[:name] = name if name
@@ -138,7 +138,7 @@ module Discorb
         parent ||= category
         payload[:parent_id] = parent.id unless parent.nil?
 
-        @client.internet.patch("/channels/#{@id}", payload).wait
+        @client.internet.patch("/channels/#{@id}", payload, audit_log_reason: reason).wait
       end
     end
 
@@ -172,7 +172,7 @@ module Discorb
     attr_reader :bitrate, :user_limit
 
     @channel_type = 2
-    def edit(name: nil, position: nil, bitrate: nil, user_limit: nil, rtc_region: nil)
+    def edit(name: nil, position: nil, bitrate: nil, user_limit: nil, rtc_region: nil, reason: nil)
       Async do
         payload = {}
         payload[:name] = name if name
@@ -181,7 +181,7 @@ module Discorb
         payload[:user_limit] = user_limit == false ? nil : user_limit unless user_limit.nil?
         payload[:rtc_region] = rtc_region == false ? nil : rtc_region unless rtc_region.nil?
 
-        @client.internet.patch("/channels/#{@id}", payload).wait
+        @client.internet.patch("/channels/#{@id}", payload, audit_log_reason: reason).wait
       end
     end
 
@@ -201,14 +201,14 @@ module Discorb
     attr_reader :bitrate, :user_limit
 
     @channel_type = 13
-    def edit(name: nil, position: nil, bitrate: nil, user_limit: nil)
+    def edit(name: nil, position: nil, bitrate: nil, user_limit: nil, reason: nil)
       Async do
         payload = {}
         payload[:name] = name if name
         payload[:position] = position if position
         payload[:bitrate] = bitrate unless bitrate.nil?
         payload[:user_limit] = user_limit unless user_limit.nil?
-        @client.internet.patch("/channels/#{@id}", payload).wait
+        @client.internet.patch("/channels/#{@id}", payload, audit_log_reason: reason).wait
       end
     end
 
@@ -286,7 +286,7 @@ module Discorb
     attr_reader :bitrate, :user_limit
 
     @channel_type = 11
-    def edit(name: nil, position: nil, bitrate: nil, user_limit: nil)
+    def edit(name: nil, position: nil, bitrate: nil, user_limit: nil, reason: nil)
       Async do
         payload = {}
         payload[:name] = name if name
@@ -294,7 +294,7 @@ module Discorb
         payload[:bitrate] = bitrate unless bitrate.nil?
         payload[:user_limit] = user_limit unless user_limit.nil?
 
-        @client.internet.patch("/channels/#{@id}", payload).wait
+        @client.internet.patch("/channels/#{@id}", payload, audit_log_reason: reason).wait
       end
     end
 
@@ -312,7 +312,7 @@ module Discorb
     attr_reader :bitrate, :user_limit
 
     @channel_type = 12
-    def edit(name: nil, position: nil, bitrate: nil, user_limit: nil)
+    def edit(name: nil, position: nil, bitrate: nil, user_limit: nil, reason: nil)
       Async do
         payload = {}
         payload[:name] = name if name
@@ -320,7 +320,7 @@ module Discorb
         payload[:bitrate] = bitrate unless bitrate.nil?
         payload[:user_limit] = user_limit unless user_limit.nil?
 
-        @client.internet.patch("/channels/#{@id}", payload).wait
+        @client.internet.patch("/channels/#{@id}", payload, audit_log_reason: reason).wait
       end
     end
 
