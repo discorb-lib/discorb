115101e8e1b22581b1ce699ecdbc380726dace0a
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-07-22 18:52:02 +0900

    Update: Improve logger example

--- examples/logger.rb
+++ examples/logger.rb
@@ -4,7 +4,7 @@
 require 'time'
 require_relative '../lib/discorb'
 
-client = Discorb::Client.new(log: $stdout, colorize_log: true)
+client = Discorb::Client.new(log: $stdout, colorize_log: true, log_level: :info)
 
 def get_log_channel(guild)
   guild.channels.find { |c| c.name == 'log' }
@@ -13,7 +13,7 @@ end
 client.on :ready do
   puts "Logged in as #{client.user}"
 end
-
+# ==== Channels ====
 client.on :channel_create do |_task, channel|
   get_log_channel(channel.guild).post embed: Discorb::Embed.new(
     'Channel Created', "Name: `#{channel.name}`\n\n#{channel}", color: Discorb::Color[:green], timestamp: Time.now
@@ -33,7 +33,7 @@ client.on :channel_delete do |_task, channel|
     'Channel Deleted', "`#{channel.name}`", color: Discorb::Color[:red], timestamp: Time.now
   )
 end
-
+# ===== Roles =====
 client.on :role_create do |_task, role|
   get_log_channel(role.guild).post embed: Discorb::Embed.new(
     'Role Created', "Name: `#{role.name}`\n\n#{role}", color: Discorb::Color[:green], timestamp: Time.now
@@ -54,4 +54,21 @@ client.on :role_delete do |_task, role|
   )
 end
 
+client.on :voice_channel_connect do |_task, state|
+  get_log_channel(state.guild).post embed: Discorb::Embed.new(
+    'Connected to VC', "User: #{state.member.mention}\nChannel: #{state.channel.mention}", color: Discorb::Color[:green]
+  )
+end
+
+client.on :voice_channel_move do |_task, before, after|
+  get_log_channel(before.guild).post embed: Discorb::Embed.new(
+    'Moved to other VC', "User: #{before.member.mention}\nChannel: #{before.channel.mention} -> #{after.channel.mention}", color: Discorb::Color[:yellow]
+  )
+end
+
+client.on :voice_channel_disconnect do |_task, state|
+  get_log_channel(state.guild).post embed: Discorb::Embed.new(
+    'Disconnected from VC', "User: #{state.member.mention}\nChannel: #{state.channel.mention}", color: Discorb::Color[:red]
+  )
+end
 client.run(ENV['discord_bot_token'])
