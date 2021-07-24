aed54988a8262e49d93629b9b27c5271bf7ba58e
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-07-24 22:44:54 +0900

    Delete: Delete not needed requires

--- lib/discorb/member.rb
+++ lib/discorb/member.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'time'
 require_relative 'user'
 
 module Discorb
--- lib/discorb/message.rb
+++ lib/discorb/message.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'time'
 require_relative 'common'
 require_relative 'member'
 require_relative 'channel'
