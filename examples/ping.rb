65258ad1722b77f90a352587917d9fab96484009
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-07-26 06:31:29 +0900

    Update: Seperate examples

--- examples/ping.rb
+++ examples/ping.rb
@@ -10,6 +10,7 @@ client = Discorb::Client.new(log: $stdout, colorize_log: true, log_level: :info,
 client.on :ready do |_task|
   puts "Logged in as #{client.user}"
 end
+
 event = client.on(:message) do |_task, message|
   next if message.author.bot?
 
@@ -18,24 +19,6 @@ event = client.on(:message) do |_task, message|
   case message.content[1..]
   when 'ping'
     message.channel.post 'Pong!'
-  when 'emtest'
-    message.channel.post(embed: Discorb::Embed.new('Embed Test', 'I am test'))
-  when /eval [\s\S+]/
-    unless message.author.id == '686547120534454315'
-      message.reply('Only onwer of this bot can use eval')
-      next
-    end
-    code = message.content.delete_prefix('!eval ').delete_prefix('```rb').delete_suffix('```')
-    res = eval("Async { |task| #{code} }", binding, __FILE__, __LINE__)  # rubocop:disable Security/Eval
-    message.add_reaction(Discorb::UnicodeEmoji['white_check_mark'])
-    unless res.nil?
-      res = res.wait if res.is_a? Async::Task
-      message.channel.post("```rb\n#{res.inspect[...1990]}\n```")
-    end
-  when 'react'
-    message.add_reaction(Discorb::UnicodeEmoji['thinking'])
-  when 'reply'
-    message.reply 'Test'
   end
 end
 event.rescue do |_task, error, message|
@@ -43,5 +26,4 @@ event.rescue do |_task, error, message|
                                           color: Discorb::Color[:red])
 end
 
-client.update_presence(Discorb::Activity.new('Music', :listening))
 client.run(ENV['discord_bot_token'])
