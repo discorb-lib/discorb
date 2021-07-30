f5a6e0dbaa32d056e6a7f4fc3aa95c833a65be41
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-07-30 09:50:01 +0900

    Add: Support emoji

--- lib/discorb/image.rb
+++ lib/discorb/image.rb
@@ -1,16 +1,26 @@
 # frozen_string_literal: true
 
 require 'base64'
+require 'mime/types'
 
 module Discorb
   class Image
-    def initialize(bytes, type)
-      @bytes = bytes
-      @type = type
+    def initialize(source, type = nil)
+      if ::File.exist?(source)
+        ::File.open(source, 'rb') do |file|
+          @bytes = file.read
+        end
+        @type = MIME::Types.type_for(source).first.to_s
+      elsif type.nil?
+        raise ArgumentError, 'File not found and type is not specified'
+      else
+        @bytes = bytes
+        @type = "image/#{type}"
+      end
     end
 
     def to_s
-      "data:image/#{@type};base64,#{Base64.strict_encode64(@bytes)}"
+      "data:#{@type};base64,#{Base64.strict_encode64(@bytes)}"
     end
   end
 end
