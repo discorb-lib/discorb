278665b542eae2e0922f89f3d90cc3bb18ce9d04
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-08-15 14:12:00 +0900

    Add: Document Components

--- lib/discorb/components.rb
+++ lib/discorb/components.rb
@@ -10,6 +10,7 @@ module Discorb
       #
       # Create a new component from hash data.
       #
+      # @see https://discord.com/developers/docs/interactions/message-components Official Discord API documentation
       # @param [Hash] data Hash data.
       #
       # @return [Component] A new component.
@@ -38,8 +39,27 @@ module Discorb
     end
   end
 
+  #
+  # Represents a button component.
+  # @!attribute [r] disabled?
+  #   Whether the button is disabled.
+  #   @return [Boolean] Whether the button is disabled.
+  #
   class Button < Component
-    attr_accessor :label, :style, :emoji, :custom_id, :url, :disabled
+    # @return [String] The label of the button.
+    attr_accessor :label
+    # @return [:primary, :secondary, :success, :danger, :link] The style of the button.
+    attr_accessor :style
+    # @return [Discorb::UnicodeEmoji, Discorb::CustomEmoji] The emoji of the button.
+    attr_accessor :emoji
+    # @return [String] The custom ID of the button.
+    #   Won't be used if the style is `:link`.
+    attr_accessor :custom_id
+    # @return [String] The URL of the button.
+    #   Only used when the style is `:link`.
+    attr_accessor :url
+    # @return [Boolean] Whether the button is disabled.
+    attr_accessor :disabled
 
     @styles = {
       primary: 1,
@@ -49,6 +69,16 @@ module Discorb
       link: 5
     }.freeze
 
+    #
+    # Initialize a new button.
+    #
+    # @param [String] label The label of the button.
+    # @param [:primary, :secondary, :success, :danger, :link] style The style of the button.
+    # @param [Discorb::UnicodeEmoji, Discorb::CustomEmoji] emoji The emoji of the button.
+    # @param [String] custom_id The custom ID of the button.
+    # @param [String] url The URL of the button.
+    # @param [Boolean] disabled Whether the button is disabled.
+    #
     def initialize(label, style = :primary, emoji: nil, custom_id: nil, url: nil, disabled: false)
       @label = label
       @style = style
@@ -62,6 +92,12 @@ module Discorb
       @disabled
     end
 
+    #
+    # Converts the button to a hash.
+    #
+    # @see https://discord.com/developers/docs/interactions/message-components#button-object-button-structure Official Discord API docs
+    # @return [Hash] A hash representation of the button.
+    #
     def to_hash
       if @style == :link
         {
@@ -84,11 +120,8 @@ module Discorb
       end
     end
 
-    def self.[](...)
-      new(...)
-    end
-
     class << self
+      # @!visibility private
       attr_reader :styles
     end
 
@@ -112,9 +145,31 @@ module Discorb
     end
   end
 
+  #
+  # Represents a select menu component.
+  #
+  # @!attribute [r] disabled?
+  #   Whether the button is disabled.
+  #   @return [Boolean] `true` if the select menu is disabled.
   class SelectMenu < Component
-    attr_accessor :custom_id, :options, :min_values, :max_values
+    # @return [String] The custom ID of the select menu.
+    attr_accessor :custom_id
+    # @return [Array<SelectMenu::Option>] The options of the select menu.
+    attr_accessor :options
+    # @return [Integer] The minimum number of values.
+    attr_accessor :min_values
+    # @return [Integer] The maximum number of values.
+    attr_accessor :max_values
 
+    #
+    # Initialize a new select menu.
+    #
+    # @param [String, Symbol] custom_id Custom ID of the select menu.
+    # @param [Array<Discorb::SelectMenu::Option>] options The options of the select menu.
+    # @param [String] placeholder The placeholder of the select menu.
+    # @param [Integer] min_values The minimum number of values.
+    # @param [Integer] max_values The maximum number of values.
+    #
     def initialize(custom_id, options, placeholder: nil, min_values: 1, max_values: 1)
       @custom_id = custom_id
       @options = options
@@ -127,6 +182,12 @@ module Discorb
       @disabled
     end
 
+    #
+    # Converts the select menu to a hash.
+    #
+    # @see https://discord.com/developers/docs/interactions/message-components#select-menu-object-select-menu-structure Official Discord API docs
+    # @return [Hash] A hash representation of the select menu.
+    #
     def to_hash
       {
         type: 3,
@@ -138,9 +199,29 @@ module Discorb
       }
     end
 
+    #
+    # Represents an option of a select menu.
+    #
     class Option
-      attr_accessor :label, :value, :description, :emoji, :default
+      # @return [String] The label of the option.
+      attr_accessor :label
+      # @return [String] The value of the option.
+      attr_accessor :value
+      # @return [String] The description of the option.
+      attr_accessor :description
+      # @return [Discorb::UnicodeEmoji, Discorb::CustomEmoji] The emoji of the option.
+      attr_accessor :emoji
+      # @return [Boolean] Whether the option is default.
+      attr_accessor :default
 
+      #
+      # Initialize a new option.
+      #
+      # @param [String] label The label of the option.
+      # @param [String] value The value of the option.
+      # @param [String] description The description of the option.
+      # @param [Discorb::UnicodeEmoji, Discorb::CustomEmoji] emoji The emoji of the option.
+      # @param [Boolean] default Whether the option is default.
       def initialize(label, value, description: nil, emoji: nil, default: false)
         @label = label
         @value = value
@@ -149,6 +230,12 @@ module Discorb
         @default = default
       end
 
+      #
+      # Converts the option to a hash.
+      #
+      # @see https://discord.com/developers/docs/interactions/message-components#select-menu-object-select-option-structure Official Discord API docs
+      # @return [Hash] Hash representation of the option.
+      #
       def to_hash
         {
           label: @label,
@@ -159,6 +246,7 @@ module Discorb
         }
       end
 
+      # @!visibility private
       def hash_emoji(emoji)
         case emoji
         when UnicodeEmoji
@@ -177,14 +265,17 @@ module Discorb
       end
 
       class << self
+        #
+        # Creates a new option from a hash.
+        #
+        # @param [Hash] data A hash representing the option.
+        #
+        # @return [Discorb::SelectMenu::Option] A new option.
+        #
         def from_hash(data)
           new(data[:label], data[:value], description: data[:description], emoji: data[:emoji], default: data[:default])
         end
       end
-    end
-
-    def self.[](...)
-      new(...)
     end
 
     private
