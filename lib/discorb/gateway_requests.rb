aa29212dc9e64f897ff506865e6f55f0606218ba
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-08-17 17:38:33 +0900

    Add: Document Activity

--- lib/discorb/gateway_requests.rb
+++ lib/discorb/gateway_requests.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-
 module Discorb
+  #
+  # Represents an activity for Gateway Command.
+  #
   class Activity
     @types = {
       playing: 0,
@@ -11,12 +13,24 @@ module Discorb
       competing: 5
     }.freeze
 
+    #
+    # Initializes a new Activity.
+    #
+    # @param [String] name The name of the activity.
+    # @param [:playing, :streaming, :listening, :watching, :competing] type The type of activity.
+    # @param [String] url The URL of the activity.
+    #
     def initialize(name, type = :playing, url = nil)
       @name = name
       @type = self.class.types[type]
       @url = url
     end
 
+    #
+    # Converts the activity to a hash.
+    #
+    # @return [Hash] A hash representation of the activity.
+    #
     def to_hash
       {
         name: @name,
@@ -26,6 +40,7 @@ module Discorb
     end
 
     class << self
+      # @!visibility private
       attr_reader :types
     end
   end
