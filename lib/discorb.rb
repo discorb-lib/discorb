21a7778755e90f48fab1169552277fb7d6ebfccd
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-08-24 13:21:07 +0900

    Add: Add macro

--- lib/discorb.rb
+++ lib/discorb.rb
@@ -6,14 +6,39 @@
 module Discorb
   # @!visibility private
   # @!macro [new] async
-  #  @note This is an asynchronous method, it will return a +Async::Task+ object.
+  #   @note This is an asynchronous method, it will return a `Async::Task` object. Use `Async::Task#wait` to get the result.
   #
   # @!macro [new] client_cache
-  #  @note This method returns an object from client cache. it will return +nil+ if the object is not in cache.
-  #  @return [nil] The object wasn't cached.
+  #   @note This method returns an object from client cache. it will return `nil` if the object is not in cache.
+  #   @return [nil] The object wasn't cached.
   #
+  # @!macro members_intent
+  #   @note This method requires `members` intent. {file:docs/Intents.md Learn more here}.
+  #
+  # @!macro presence_intent
+  #   @note This method requires `presence` intent. {file:docs/Intents.md Learn more here}.
+  #
+  # @!macro edit
+  #   @note The arguments of this method are defaultly set to `:unset`. Specify value to set the value, if not don't specify or specify `:unset`.
+  #
+  # @!macro http
+  #   @note This method calls HTTP request.
+  #   @raise [Discorb::HTTPError] HTTP request failed.
+  #   
   def macro
     # NOTE: this method is only for YARD.
+    puts 'Wow, You found the easter egg!\n\n'
+    red = "\e[31m"
+    reset = "\e[m"
+    puts <<~"EASTEREGG"
+        | .               #{red}   |     #{reset}
+      __|                 #{red}   |__   #{reset}
+     /  | |  __   __  _   #{red} _ |  \\  #{reset}
+    (   | | (__  /   / \\ #{red}|/  |   ) #{reset}
+     \\__| |  __) \\__ \\_/ #{red}|   |__/  #{reset}
+
+          https://github.com/discorb-lib/discorb
+    EASTEREGG
   end
 end
 require_order = %w[common flag dictionary error internet intents emoji_table modules] +
