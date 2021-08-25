0e80712a0dc9edddd1485e0d02a055a3abbd47cd
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-08-26 07:26:27 +0900

    Add: Document Time#to_df

--- lib/discorb/extend.rb
+++ lib/discorb/extend.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class Time
+  #
+  # Format a time object to a Discord formatted string.
+  #
+  # @param ["f", "F", "d", "D", "t", "T", "R"] format The format to use.
+  #
+  # @return [String] The formatted time.
+  #
   def to_df(type = nil)
     if type.nil?
       "<t:#{to_i}>"
