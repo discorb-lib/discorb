b5f48d55fcc6366b2b300b1563a8493fbb1ce669
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-08-26 07:59:22 +0900

    Add: Document Flag

--- lib/discorb/flag.rb
+++ lib/discorb/flag.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 module Discorb
+  #
+  # Represents a flag.
+  # @abstract
+  #
   class Flag
-    attr_reader :values, :value
+    # @return [Hash{Symbol => Boolean}] the values of the flag.
+    attr_reader :values
+    # @return [Integer] the value of the flag.
+    attr_reader :value
 
     @bits = {}
 
+    # @!visibility private
     def initialize(value)
       @value = value
       @values = {}
@@ -26,29 +34,70 @@ module Discorb
       @values.key?(name.to_s.delete_suffix("?").to_sym) ? true : super
     end
 
-    def +(other)
-      self.class.new(@value | other.value)
-    end
-
-    def -(other)
-      self.class.new(@value & (@value ^ other.value))
-    end
-
+    #
+    # Union of two flags.
+    #
+    # @param [Discorb::Flag] other The other flag.
+    #
+    # @return [Discorb::Flag] The union of the two flags.
+    #
     def |(other)
       self.class.new(@value | other.value)
     end
 
+    alias + |
+
+    #
+    # Subtraction of two flags.
+    #
+    # @param [Discorb::Flag] other The other flag.
+    #
+    # @return [Discorb::Flag] The subtraction of the two flags.
+    #
+    def -(other)
+      self.class.new(@value & (@value ^ other.value))
+    end
+
+    #
+    # Intersection of two flags.
+    #
+    # @param [Discorb::Flag] other The other flag.
+    #
+    # @return [Discorb::Flag] The intersection of the two flags.
+    #
     def &(other)
       self.class.new(@value & other.value)
     end
 
+    #
+    # XOR of two flags.
+    #
+    # @param [Discorb::Flag] other The other flag.
+    #
+    # @return [Discorb::Flag] The XOR of the two flags.
+    #
     def ^(other)
       self.class.new(@value ^ other.value)
     end
 
+    #
+    # Negation of the flag.
+    #
+    # @return [Discorb::Flag] The negation of the flag.
+    #
+    def ~@
+      self.class.new(~@value)
+    end
+
     class << self
+      # @return [Hash{Integer => Symbol}] the bits of the flag.
       attr_reader :bits
 
+      #
+      # Max value of the flag.
+      #
+      # @return [Integer] the max value of the flag.
+      #
       def max_value
         2 ** @bits.values.max - 1
       end
