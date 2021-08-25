b4ced43103184b51f05fade7389307ee25187c4f
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-08-26 07:33:49 +0900

    Add: Document Extension

--- lib/discorb/extension.rb
+++ lib/discorb/extension.rb
@@ -1,18 +1,54 @@
 # frozen_string_literal: true
 
 module Discorb
+  #
+  # Module to make extension.
+  # extend this module to make your own extension.
+  # @see file:docs/extension.md
+  # @abstract
+  #
   module Extension
     @events = {}
     @client = nil
 
+    #
+    # Define a new event.
+    #
+    # @param [Symbol] event_name The name of the event.
+    # @param [Symbol] id The id of the event. Used to delete the event.
+    # @param [Hash] discriminator Other discriminators.
+    # @param [Proc] block The block to execute when the event is triggered.
+    #
+    # @return [Discorb::Event] The event.
+    #
     def event(event_name, id: nil, **discriminator, &block)
+      raise ArgumentError, "Event name must be a symbol" unless event_name.is_a?(Symbol)
+      raise ArgumentError, "block must be a Proc" unless block.is_a?(Proc)
+
       @events = {} if @events.nil?
-      @events[event_name] = [] if @events[event_name].nil?
+      @events[event_name] ||= []
       discriminator[:extension] = Extension
       @events[event_name] << Discorb::Event.new(block, id, discriminator)
     end
 
+    #
+    # Define a new once event.
+    #
+    # @param [Symbol] event_name The name of the event.
+    # @param [Symbol] id The id of the event. Used to delete the event.
+    # @param [Hash] discriminator Other discriminators.
+    # @param [Proc] block The block to execute when the event is triggered.
+    #
+    # @return [Discorb::Event] The event.
+    #
+    def once_event(event_name, id: nil, **discriminator, &block)
+      event(event_name, id: id, once: true, **discriminator, &block)
+    end
+
+    # @return [Hash{Symbol => Array<Discorb::Event>}] The events of the extension.
     attr_reader :events
+
+    # @!visibility private
     attr_accessor :client
   end
 end
