4d075d5bdc9c47cafde8956af80e0ac788813234
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-08-26 17:17:42 +0900

    Add: Document Image

--- lib/discorb/image.rb
+++ lib/discorb/image.rb
@@ -4,21 +4,38 @@ require "base64"
 require "mime/types"
 
 module Discorb
+  #
+  # Represents an image.
+  #
   class Image
+    #
+    # Initializes a new Image.
+    #
+    # @param [#read] source The source of the image.
+    # @param [String] type The MIME type of the image.
+    # @overload
+    #   @param [String] source The file path of the source.
+    #   @param [String] type The MIME type of the image.
+    #
     def initialize(source, type = nil)
-      if ::File.exist?(source)
+      if source.respond_to?(:read)
+        @bytes = source.read
+        @type = type || MIME::Types.type_for(source.path).first.content_type
+      elsif ::File.exist?(source)
         ::File.open(source, "rb") do |file|
           @bytes = file.read
         end
         @type = MIME::Types.type_for(source).first.to_s
-      elsif type.nil?
-        raise ArgumentError, "File not found and type is not specified"
       else
-        @bytes = bytes
-        @type = "image/#{type}"
+        raise ArgumentError, "Couldn't read file."
       end
     end
 
+    #
+    # Formats the image as a Discord style.
+    #
+    # @return [String] The image as a Discord style.
+    #
     def to_s
       "data:#{@type};base64,#{Base64.strict_encode64(@bytes)}"
     end
