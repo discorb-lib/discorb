e4e7263fa11424d575cb1de0f1c89a411e373633
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-08-27 11:50:45 +0900

    Add: Document Interaction

--- lib/discorb/interaction.rb
+++ lib/discorb/interaction.rb
@@ -1,12 +1,38 @@
 # frozen_string_literal: true
 
 module Discorb
+  #
+  # Represents a user interaction with the bot.
+  # @!attribute [r] guild
+  #   @macro client_cache
+  #   @return [Discorb::Guild] The guild the interaction took place in.
+  # @!attribute [r] channel
+  #   @macro client_cache
+  #   @return [Discorb::Channel] The channel the interaction took place in.
+  # @!attribute [r] target
+  #   @return [Discorb::User, Discorb::Member] The user or member the interaction took place with.
+  #
   class Interaction < DiscordModel
-    attr_reader :id, :application_id, :type, :member, :user, :version, :token
+    # @return [Discorb::Snowflake] The ID of the interaction.
+    attr_reader :id
+    # @return [Discorb::Snowflake] The ID of the application that created the interaction.
+    attr_reader :application_id
+    # @return [Symbol] The type of interaction.
+    attr_reader :type
+    # @return [Discorb::Member] The member that created the interaction.
+    attr_reader :member
+    # @return [Discorb::User] The user that created the interaction.
+    attr_reader :user
+    # @return [Integer] The type of interaction.
+    # @note This is always `1` for now.
+    attr_reader :version
+    # @return [String] The token for the interaction.
+    attr_reader :token
 
     @interaction_type = nil
     @interaction_name = nil
 
+    # @!visibility private
     def initialize(client, data)
       @client = client
       @id = Snowflake.new(data[:id])
@@ -19,6 +45,8 @@ module Discorb
       @user = @client.users[data[:user][:id]] || User.new(@client, data[:user]) if data[:user]
       @token = data[:token]
       @version = data[:version]
+      @defered = false
+      @responded = false
       _set_data(data[:data])
     end
 
@@ -30,17 +58,21 @@ module Discorb
       @client.channels[@channel_id]
     end
 
-    def fired_by
+    def target
       @member || @user
     end
+
+    alias fired_by target
 
     def inspect
       "#<#{self.class} id=#{@id}>"
     end
 
     class << self
+      # @!visibility private
       attr_reader :interaction_type, :interaction_name, :event_name
 
+      # @!visibility private
       def make_interaction(client, data)
         descendants.each do |klass|
           return klass.make_interaction(client, data) if !klass.interaction_type.nil? && klass.interaction_type == data[:type]
@@ -49,24 +81,44 @@ module Discorb
         Interaction.new(client, data)
       end
 
+      # @!visibility private
       def descendants
         ObjectSpace.each_object(Class).select { |klass| klass < self }
       end
     end
 
+    #
+    # A module for response with source.
+    #
     module SourceResponse
+      #
+      # Response with `DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE`(`5`).
+      #
+      # @param [Boolean] hide Whether to hide the response (ephemeral).
+      #
       def defer_source(hide: false)
         Async do
-          @client.internet.post("/interactions/#{@id}/#{@token}/callback",
-                                {
-                                  type: 5,
-                                  data: {
-                                    flags: (hide ? 1 << 6 : 0),
-                                  },
-                                }).wait
+          @client.internet.post("/interactions/#{@id}/#{@token}/callback", {
+            type: 5,
+            data: {
+              flags: (hide ? 1 << 6 : 0),
+            },
+          }).wait
+          @defered = true
         end
       end
 
+      #
+      # Response with `CHANNEL_MESSAGE_WITH_SOURCE`(`4`).
+      #
+      # @param [String] content The content of the response.
+      # @param [Boolean] tts Whether to send the message as text-to-speech.
+      # @param [Discorb::Embed] embed The embed to send.
+      # @param [Array<Discorb::Embed>] embeds The embeds to send. (max: 10)
+      # @param [Discorb::AllowedMentions] allowed_mentions The allowed mentions to send.
+      # @param [Array<Discorb::Components>, Array<Array<Discorb::Components>>] components The components to send.
+      # @param [Boolean] hide Whether to hide the response (ephemeral).
+      #
       def post(content, tts: false, embed: nil, embeds: nil, allowed_mentions: nil, components: nil, hide: false)
         payload = {}
         payload[:content] = content if content
@@ -99,24 +151,49 @@ module Discorb
           payload[:components] = tmp_components.filter { |c| c.length.positive? }.map { |c| { type: 1, components: c.map(&:to_hash) } }
         end
         payload[:flags] = (hide ? 1 << 6 : 0)
-        @client.internet.post("/interactions/#{@id}/#{@token}/callback", { type: 4, data: payload }).wait
+        if @responded
+          @client.internet.post("/webhooks/#{@id}/#{@token}", { type: 4, data: payload }).wait
+        elsif @defered
+          @client.internet.post("/interactions/#{@id}/#{@token}/@original/edit", { type: 4, data: payload }).wait
+        else
+          @client.internet.post("/interactions/#{@id}/#{@token}/callback", { type: 4, data: payload }).wait
+        end
+        @responded = true
       end
     end
 
+    #
+    # A module for response with update.
+    #
     module UpdateResponse
+      #
+      # Response with `DEFERRED_UPDATE_MESSAGE`(`6`).
+      #
+      # @param [Boolean] hide Whether to hide the response (ephemeral).
+      #
       def defer_update(hide: false)
         Async do
-          @client.internet.post("/interactions/#{@id}/#{@token}/callback",
-                                {
-                                  type: 7,
-                                  data: {
-                                    flags: (hide ? 1 << 6 : 0),
-                                  },
-                                }).wait
+          @client.internet.post("/interactions/#{@id}/#{@token}/callback", {
+            type: 7,
+            data: {
+              flags: (hide ? 1 << 6 : 0),
+            },
+          }).wait
         end
       end
 
-      def post(content, tts: false, embed: nil, embeds: nil, allowed_mentions: nil, components: nil, hide: false)
+      #
+      # Response with `UPDATE_MESSAGE`(`7`).
+      #
+      # @param [String] content The content of the response.
+      # @param [Boolean] tts Whether to send the message as text-to-speech.
+      # @param [Discorb::Embed] embed The embed to send.
+      # @param [Array<Discorb::Embed>] embeds The embeds to send. (max: 10)
+      # @param [Discorb::AllowedMentions] allowed_mentions The allowed mentions to send.
+      # @param [Array<Discorb::Components>, Array<Array<Discorb::Components>>] components The components to send.
+      # @param [Boolean] hide Whether to hide the response (ephemeral).
+      #
+      def edit(content, tts: false, embed: nil, embeds: nil, allowed_mentions: nil, components: nil, hide: false)
         payload = {}
         payload[:content] = content if content
         payload[:tts] = tts
@@ -159,6 +236,10 @@ module Discorb
     end
   end
 
+  #
+  # Represents a slash command interaction.
+  # @todo Implement this.
+  #
   class SlashCommandInteraction < Interaction
     @interaction_type = 2
     @interaction_name = :slash_command
@@ -168,21 +249,30 @@ module Discorb
     end
   end
 
+  #
+  # Represents a message component interaction.
+  # @abstract
+  #
   class MessageComponentInteraction < Interaction
     include Interaction::SourceResponse
-    attr_reader :component_type, :custom_id, :values
+    include Interaction::UpdateResponse
+    # @return [String] The content of the response.
+    attr_reader :custom_id
 
     @interaction_type = 3
     @interaction_name = :message_component
 
+    # @!visibility private
     def initialize(client, data)
       super
       @message = Message.new(@client, data[:message].merge({ member: data[:member] }))
     end
 
     class << self
+      # @!visibility private
       attr_reader :component_type
 
+      # @!visibility private
       def make_interaction(client, data)
         nested_classes.each do |klass|
           return klass.new(client, data) if !klass.component_type.nil? && klass.component_type == data[:type]
@@ -191,14 +281,19 @@ module Discorb
         MessageComponentInteraction.new(client, data)
       end
 
+      # @!visibility private
       def nested_classes
         constants.select { |c| const_get(c).is_a? Class }.map { |c| const_get(c) }
       end
     end
 
+    #
+    # Represents a button interaction.
+    #
     class Button < MessageComponentInteraction
       @component_type = 2
       @event_name = :button_click
+      # @return [String] The custom id of the button.
       attr_reader :custom_id
 
       private
@@ -208,10 +303,18 @@ module Discorb
       end
     end
 
+    #
+    # Represents a select menu interaction.
+    # @!attribute [r] value
+    #   @return [String] The first selected value.
+    #
     class SelectMenu < MessageComponentInteraction
       @component_type = 3
       @event_name = :select_menu_select
-      attr_reader :custom_id, :values
+      # @return [String] The custom id of the select menu.
+      attr_reader :custom_id
+      # @return [Array<String>] The selected options.
+      attr_reader :values
 
       def value
         @values[0]
