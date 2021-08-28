c975304f6825a89e02c35420ffdcfdd551ba6dc2
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-08-28 13:16:48 +0900

    Add: Document Sticker

--- lib/discorb/sticker.rb
+++ lib/discorb/sticker.rb
@@ -1,8 +1,32 @@
 # frozen_string_literal: true
 
 module Discorb
+  #
+  # Represents a sticker.
+  #
   class Sticker < DiscordModel
-    attr_reader :id, :name, :tags, :type, :format, :description, :pack_id, :sort_value, :guild_id, :user, :available
+    # @return [Discorb::Snowflake] The ID of the sticker.
+    attr_reader :id
+    # @return [String] The name of the sticker.
+    attr_reader :name
+    # @return [Array<String>] The tags of the sticker.
+    attr_reader :tags
+    # @return [:official, :guild] The type of sticker.
+    attr_reader :type
+    # @return [:png, :apng, :lottie] The format of the sticker.
+    attr_reader :format
+    # @return [String] The URL of the sticker.
+    attr_reader :description
+    # @return [Discorb::Sticker] The ID of the sticker pack.
+    attr_reader :pack_id
+    # @return [Integer] The sort value of the sticker.
+    attr_reader :sort_value
+    # @return [Discorb::Snowflake] The ID of the guild the sticker is in.
+    attr_reader :guild_id
+    # @return [Discorb::User] The user who created the sticker.
+    attr_reader :user
+    # @return [Boolean] Whether the sticker is available.
+    attr_reader :available
     alias available? available
 
     @sticker_type = {
@@ -20,23 +44,48 @@ module Discorb
       _set_data(data)
     end
 
+    #
+    # Represents a sticker of guilds.
+    #
     class GuildSticker < Sticker
+      # @!attribute [r] guild
+      #   @macro client_cache
+      #   @return [Discorb::Guild] The guild the sticker is in.
+
       def guild
         @client.guilds[@guild_id]
       end
 
-      def edit(name: nil, description: nil, tag: nil, reason: nil)
+      #
+      # Edits the sticker.
+      # @macro async
+      # @macro http
+      # @macro edit
+      #
+      # @param [String] name The new name of the sticker.
+      # @param [String] description The new description of the sticker.
+      # @param [Discorb::Emoji] tags The new tags of the sticker.
+      # @param [String] reason The reason for the edit.
+      #
+      def edit(name: :unset, description: :unset, tag: :unset, reason: :unset)
         Async do
           payload = {}
-          payload[:name] = name if name
-          payload[:description] = description if description
-          payload[:tags] = tag.name if tag
+          payload[:name] = name unless name == :unset
+          payload[:description] = description unless description == :unset
+          payload[:tags] = tag.name unless tag == :unset
           @client.internet.patch("/guilds/#{@guild_id}/stickers/#{@id}", payload, audit_log_reason: reason).wait
         end
       end
 
       alias modify edit
 
+      #
+      # Deletes the sticker.
+      # @macro async
+      # @macro http
+      #
+      # @param [String] reason The reason for the deletion.
+      #
       def delete!(reason: nil)
         Async do
           @client.internet.delete("/guilds/#{@guild_id}/stickers/#{@id}", audit_log_reason: reason).wait
@@ -46,9 +95,28 @@ module Discorb
       alias destroy! delete!
     end
 
+    #
+    # Represents a sticker pack.
+    #
     class Pack < DiscordModel
-      attr_reader :id, :name, :sku_id, :cover_sticker_id, :description, :banner_asset_id, :stickers, :banner
+      # @return [Discorb::Snowflake] The ID of the sticker pack.
+      attr_reader :id
+      # @return [String] The name of the sticker pack.
+      attr_reader :name
+      # @return [Discorb::Snowflake] The ID of the SKU.
+      attr_reader :sku_id
+      # @return [Discorb::Snowflake] The cover sticker of the pack.
+      attr_reader :cover_sticker_id
+      # @return [String] The description of the pack.
+      attr_reader :description
+      # @return [Discorb::Store::SKU] The banner asset ID of the pack.
+      attr_reader :banner_asset_id
+      # @return [Array<Discorb::Sticker>] The stickers in the pack.
+      attr_reader :stickers
+      # @return [Discorb::Asset] The banner of the pack.
+      attr_reader :banner
 
+      # @!visibility private
       def initialize(client, data)
         @client = client
         @id = Snowflake.new(data[:id])
@@ -82,6 +150,7 @@ module Discorb
     end
 
     class << self
+      # @!visibility private
       attr_reader :sticker_type, :sticker_format
     end
   end
