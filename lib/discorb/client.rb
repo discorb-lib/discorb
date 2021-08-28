80bd44f7a58043d1165c670675bb9dafa5b657ef
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-08-28 16:21:09 +0900

    Add: Document Client

--- lib/discorb/client.rb
+++ lib/discorb/client.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require "pp"
 require "json"
 require "logger"
 
@@ -8,11 +7,52 @@ require "async"
 require "async/websocket/client"
 
 module Discorb
+  #
+  # Class for connecting to the Discord server.
+  #
   class Client
+    # @return [Discorb::Intents] The intents that the client is currently using.
     attr_accessor :intents
-    attr_reader :application, :internet, :heartbeat_interval, :api_version, :token, :allowed_mentions, :user, :guilds, :users,
-                :channels, :emojis, :messages, :log
+    # @return [Discorb::Application] The application that the client is using.
+    # @return [nil] If never fetched application by {#fetch_application}.
+    attr_reader :application
+    # @return [Discorb::Internet] The internet client.
+    attr_reader :internet
+    # @return [Integer] The heartbeat interval.
+    attr_reader :heartbeat_interval
+    # @return [Integer] The API version of the Discord gateway.
+    # @return [nil] If not connected to the gateway.
+    attr_reader :api_version
+    # @return [String] The token of the client.
+    attr_reader :token
+    # @return [Discorb::AllowedMentions] The allowed mentions that the client is using.
+    attr_reader :allowed_mentions
+    # @return [Discorb::ClientUser] The client user.
+    attr_reader :user
+    # @return [Discorb::Dictionary{Discorb::Snowflake => Discorb::Guild}] A dictionary of guilds.
+    attr_reader :guilds
+    # @return [Discorb::Dictionary{Discorb::Snowflake => Discorb::User}] A dictionary of users.
+    attr_reader :users
+    # @return [Discorb::Dictionary{Discorb::Snowflake => Discorb::Channel}] A dictionary of channels.
+    attr_reader :channels
+    # @return [Discorb::Dictionary{Discorb::Snowflake => Discorb::Emoji}] A dictionary of emojis.
+    attr_reader :emojis
+    # @return [Discorb::Dictionary{Discorb::Snowflake => Discorb::Message}] A dictionary of messages.
+    attr_reader :messages
+    # @return [Discorb::Logger] The logger.
+    attr_reader :log
 
+    #
+    # Initializes a new client.
+    #
+    # @param [Discorb::AllowedMentions] allowed_mentions The allowed mentions that the client is using.
+    # @param [Discorb::Intents] intents The intents that the client is currently using.
+    # @param [Integer] message_caches The number of messages to cache.
+    # @param [#puts] log The IO object to use for logging.
+    # @param [Boolean] colorize_log Whether to colorize the log.
+    # @param [:debug, :info, :warn, :error, :critical] log_level The log level.
+    # @param [Boolean] wait_until_ready Whether to delay event dispatch until ready.
+    #
     def initialize(allowed_mentions: nil, intents: nil, message_caches: 1000, log: nil, colorize_log: false, log_level: :info, wait_until_ready: true)
       @allowed_mentions = allowed_mentions || AllowedMentions.new(everyone: true, roles: true, users: true)
       @intents = (intents or Intents.default)
@@ -34,6 +74,17 @@ module Discorb
       @conditions = {}
     end
 
+    #
+    # Registers an event handler.
+    # @see file:docs/Events.md
+    #
+    # @param [Symbol] event_name The name of the event.
+    # @param [Symbol] id Custom ID of the event.
+    # @param [Hash] discriminator The discriminator of the event.
+    # @param [Proc] block The block to execute when the event is triggered.
+    #
+    # @return [Discorb::Event] The event.
+    #
     def on(event_name, id: nil, **discriminator, &block)
       ne = Event.new(block, id, discriminator)
       @events[event_name] ||= []
@@ -41,6 +92,13 @@ module Discorb
       ne
     end
 
+    #
+    # Almost same as {#on}, but only triggers the event once.
+    #
+    # @param (see #on)
+    #
+    # @return [Discorb::Event] The event.
+    #
     def once(event_name, id: nil, **discriminator, &block)
       discriminator[:once] = true
       ne = Event.new(block, id, discriminator)
@@ -49,10 +107,22 @@ module Discorb
       ne
     end
 
+    #
+    # Remove event by ID.
+    #
+    # @param [Symbol] event_name The name of the event.
+    # @param [Symbol] id The ID of the event.
+    #
     def remove_event(event_name, id)
       @events[event_name].delete_if { |e| e.id == id }
     end
 
+    #
+    # Dispatch an event.
+    #
+    # @param [Symbol] event_name The name of the event.
+    # @param [Object] args The arguments to pass to the event.
+    #
     def dispatch(event_name, *args)
       Async do |_task|
         if (conditions = @conditions[event_name])
@@ -94,11 +164,27 @@ module Discorb
       end
     end
 
+    #
+    # Starts the client.
+    #
+    # @param [String] token The token to use.
+    #
     def run(token)
       @token = token.to_s
       connect_gateway(true)
     end
 
+    #
+    # Fetch user from ID.
+    # @macro async
+    # @macro http
+    #
+    # @param [#to_s] id <description>
+    #
+    # @return [Discorb::User] The user.
+    #
+    # @raise [Discorb::NotFoundError] If the user doesn't exist.
+    #
     def fetch_user(id)
       Async do
         _resp, data = internet.get("/users/#{id}").wait
@@ -106,6 +192,17 @@ module Discorb
       end
     end
 
+    #
+    # Fetch channel from ID.
+    # @macro async
+    # @macro http
+    #
+    # @param [#to_s] id The ID of the channel.
+    #
+    # @return [Discorb::Channel] The channel.
+    #
+    # @raise [Discorb::NotFoundError] If the channel doesn't exist.
+    #
     def fetch_channel(id)
       Async do
         _resp, data = internet.get("/channels/#{id}").wait
@@ -113,6 +210,17 @@ module Discorb
       end
     end
 
+    #
+    # Fetch guild from ID.
+    # @macro async
+    # @macro http
+    #
+    # @param [#to_s] id <description>
+    #
+    # @return [Discorb::Guild] The guild.
+    #
+    # @raise [Discorb::NotFoundError] If the guild doesn't exist.
+    #
     def fetch_guild(id)
       Async do
         _resp, data = internet.get("/guilds/#{id}").wait
@@ -120,6 +228,17 @@ module Discorb
       end
     end
 
+    #
+    # Fetch invite from code.
+    # @macro async
+    # @macro http
+    #
+    # @param [String] code The code of the invite.
+    # @param [Boolean] with_count Whether to include the count of the invite.
+    # @param [Boolean] with_expiration Whether to include the expiration of the invite.
+    #
+    # @return [Discorb::Invite] The invite.
+    #
     def fetch_invite(code, with_count: false, with_expiration: false)
       Async do
         _resp, data = internet.get("/invites/#{code}?with_count=#{with_count}&with_expiration=#{with_expiration}").wait
@@ -127,6 +246,16 @@ module Discorb
       end
     end
 
+    #
+    # Fetch webhook from ID.
+    # If application was cached, it will be used.
+    # @macro async
+    # @macro http
+    #
+    # @param [Boolean] force Whether to force the fetch.
+    #
+    # @return [Discorb::Application] The application.
+    #
     def fetch_application(force: false)
       Async do
         next @application if @application && !force
@@ -137,6 +266,13 @@ module Discorb
       end
     end
 
+    #
+    # Fetch nitro sticker pack from ID.
+    # @macro async
+    # @macro http
+    #
+    # @return [Array<Discorb::Sticker::Pack>] The packs.
+    #
     def fetch_nitro_sticker_packs
       Async do
         _resp, data = internet.get("/stickers-packs").wait
@@ -144,16 +280,18 @@ module Discorb
       end
     end
 
-    def update_presence(activity = nil, activities: nil, idle: nil, status: nil, afk: nil)
+    #
+    # Update presence of the client.
+    #
+    # @param [Discorb::Activity] activity The activity to update.
+    # @param [:online, :idle, :dnd, :invisible] status The status to update.
+    #
+    def update_presence(activity = nil, activity: nil, status: nil)
       payload = {}
       if !activity.nil?
         payload[:activities] = [activity.to_hash]
-      elsif !activities.nil?
-        payload[:activities] = activities.map(&:to_hash)
       end
-      payload[:idle] = (Time.now.to_f * 1000).floor unless idle.nil?
       payload[:status] = status unless status.nil?
-      payload[:afk] = afk unless afk.nil?
       if @connection
         Async do |_task|
           send_gateway(3, **payload)
@@ -165,6 +303,17 @@ module Discorb
 
     alias change_presence update_presence
 
+    #
+    # Method to wait for a event.
+    #
+    # @param [Symbol] event_name The name of the event.
+    # @param [Integer] timeout The timeout in seconds.
+    # @param [Proc] check The check to use.
+    #
+    # @return [Object] The result of the event.
+    #
+    # @raise [Discorb::TimeoutError] If the event didn't occur in time.
+    #
     def event_lock(event, timeout = nil, &check)
       Async do |task|
         condition = Async::Condition.new
@@ -191,6 +340,11 @@ module Discorb
       "#<#{self.class} user=\"#{user}\">"
     end
 
+    #
+    # Load the extension.
+    #
+    # @param [Module] mod The extension to load.
+    #
     def extend(mod)
       if mod.respond_to?(:events)
         mod.events.each do |name, events|
@@ -205,8 +359,5 @@ module Discorb
     end
 
     include Discorb::GatewayHandler
-
-    alias add_event on
-    alias event on
   end
 end
