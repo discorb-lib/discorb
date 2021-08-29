325b0cb45e877e77df0c66623f690e110f139fb4
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-08-29 16:46:18 +0900

    Update: Update version

--- lib/discorb/common.rb
+++ lib/discorb/common.rb
@@ -4,7 +4,7 @@ module Discorb
   # @return [String] The API base URL.
   API_BASE_URL = "https://discord.com/api/v9"
   # @return [String] The version of discorb.
-  VERSION = "0.0.7"
+  VERSION = "0.0.8"
   # @return [String] The user agent for the bot.
   USER_AGENT = "DiscordBot (https://github.com/discorb-lib/discorb #{VERSION}) Ruby/#{RUBY_VERSION}"
 
