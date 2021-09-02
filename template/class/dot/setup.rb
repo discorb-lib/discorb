e78dd14f4bb55698d0bccf9facd2d9b4e690a441
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-09-02 19:15:32 +0900

    Fix: Fix branch

--- template/class/dot/setup.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-include T("templates/module/dot")
-
-def init
-  super
-  sections.push :superklass
-end
--- template/root/dot/setup.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-include T("templates/module/dot")
-
-def format_path(_object)
-  ""
-end
