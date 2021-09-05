666974f4316143f23309ca9e5ab3754d5cae24d9
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-09-06 06:23:05 +0900

    Rename: Rename version

---
+++ lib/discorb/exe/show.rb
@@ -0,0 +1,8 @@
+# description: Show information of your environment.
+
+require "etc"
+require "discorb"
+puts "\e[90mRuby:\e[m #{RUBY_VERSION}"
+puts "\e[90mdiscorb:\e[m #{Discorb::VERSION}"
+uname = Etc.uname
+puts "\e[90mSystem:\e[m #{uname[:sysname]} #{uname[:release]}"
--- lib/discorb/exe/version.rb
+++
@@ -1,8 +0,0 @@
-# description: Show information of your environment.
-
-require "etc"
-require "discorb"
-puts "Ruby: #{RUBY_VERSION}"
-puts "discorb: #{Discorb::VERISON}"
-uname = Etc.uname
-puts "System: #{uname[:sysname]} #{uname[:release]}"
