f7a19fcc4cc24479c6884ddfe878bb1494dc1b6c
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-09-24 18:11:29 +0900

    Revert: Back to true, true

--- lib/discorb/client.rb
+++ lib/discorb/client.rb
@@ -502,7 +502,7 @@ module Discorb
           iputs "Process ID: #{Process.pid}"
           iputs "Title: #{title}"
 
-          Process.daemon(true, false)
+          Process.daemon(true, true)
         end
       end
     end
