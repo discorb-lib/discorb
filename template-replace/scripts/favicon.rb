117fe2c03af47676575656246f7118640d5c8296
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-09-25 23:06:43 +0900

    Fix: Fix path

--- template-replace/scripts/favicon.rb
+++ template-replace/scripts/favicon.rb
@@ -3,7 +3,7 @@ require "fileutils"
 def add_favicon(dir)
   Dir.glob("#{dir}/**/*.html").each do |file|
     content = File.read(file)
-    content.gsub!(/<head>/, "<head>\n<link rel=\"shortcut icon\" href=\"favicon.png\" />")
+    content.gsub!(/<head>/, "<head>\n<link rel=\"shortcut icon\" href=\"/favicon.png\" />")
     File.write(file, content)
   end
 end
