34796e64b603a0c4ccf9b5f145243e825e009e8e
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-09-26 14:09:42 +0900

    Fix: Fix Message Menu Command

--- lib/discorb/interaction.rb
+++ lib/discorb/interaction.rb
@@ -418,7 +418,7 @@ module Discorb
       private
 
       def _set_data(data)
-        @target = Message.new(@client, data[:resolved][:messages][data[:target_id].to_sym])
+        @target = Message.new(@client, data[:resolved][:messages][data[:target_id].to_sym].merge(guild_id: @guild_id))
         @client.commands.find { |c| c.name == data[:name] && c.type_raw == 3 }.block.call(self, @target)
       end
     end
