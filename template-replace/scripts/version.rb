90b50c9ab3755980ce3b189c321ef3f00aaaabd4
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-10-02 17:38:44 +0900

    Update: Format code

--- template-replace/scripts/version.rb
+++ template-replace/scripts/version.rb
@@ -12,16 +12,28 @@ def build_version_sidebar(dir, version)
     if tag_version == version
       cls += " current"
     end
-    res += template.gsub("!version!", tag_version).gsub("!path!", "/" + version).gsub("!class!", cls).gsub("!sha!", sha)
+    res += template
+      .gsub("!version!", tag_version)
+      .gsub("!path!", "/" + tag_version)
+      .gsub("!class!", cls)
+      .gsub("!sha!", sha)
   end
   i += 1
   cls = i % 2 == 0 ? "even" : "odd"
   if version == "main"
     cls += " current"
   end
-  res.insert 0, template.gsub("!version!", "Main").gsub("!path!", "/main").gsub("!class!", cls).gsub("!sha!", "Latest on GitHub")
+  res.insert 0, template
+               .gsub("!version!", "Main")
+               .gsub("!path!", "/main")
+               .gsub("!class!", cls)
+               .gsub("!sha!", "Latest on GitHub")
   i += 1
   cls = i % 2 == 0 ? "even" : "odd"
-  res.insert 0, template.gsub("!version!", "Latest").gsub("!path!", "/").gsub("!class!", cls).gsub("!sha!", "Latest on RubyGems")
+  res.insert 0, template
+               .gsub("!version!", "Latest")
+               .gsub("!path!", "/")
+               .gsub("!class!", cls)
+               .gsub("!sha!", "Latest on RubyGems")
   File.write(dir + "/version_list.html", raw.gsub("<!--replace-->", res))
 end
