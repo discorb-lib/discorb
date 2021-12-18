45d4d4071c8f9c1b9f6391a91cb6e959ade04aed
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-12-18 21:50:37 +0900

    Add: Add platform info

--- lib/discorb/exe/show.rb
+++ lib/discorb/exe/show.rb
@@ -2,7 +2,9 @@
 
 require "etc"
 require "discorb"
+
 puts "\e[90mRuby:\e[m #{RUBY_VERSION}"
 puts "\e[90mdiscorb:\e[m #{Discorb::VERSION}"
 uname = Etc.uname
 puts "\e[90mSystem:\e[m #{uname[:sysname]} #{uname[:release]}"
+puts "\e[90mPlatform:\e[m #{RUBY_PLATFORM}"
