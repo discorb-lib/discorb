d7ec888006978a024bc70da9c56a9da40530fb0e
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2022-02-14 21:56:16 +0900

    Add: Add component spec

---
+++ spec/components/select-menu.spec.rb
@@ -0,0 +1,26 @@
+require "discorb"
+
+RSpec.describe Discorb::Button do
+  it "creates a select menu" do
+    expect {
+      Discorb::SelectMenu.new("menu", [
+        Discorb::SelectMenu::Option.new("label", "value"),
+      ])
+    }.not_to raise_error
+  end
+  it "converts to payload" do
+    expect(Discorb::SelectMenu.new("menu", [
+      Discorb::SelectMenu::Option.new("label", "value"),
+    ]).to_hash).to eq(
+      {
+        custom_id: "menu",
+        disabled: nil,
+        max_values: 1,
+        min_values: 1,
+        options: [{ :default => false, :description => nil, :emoji => nil, :label => "label", :value => "value" }],
+        placeholder: nil,
+        type: 3,
+      }
+    )
+  end
+end
---
+++ spec/components/text-input.spec.rb
@@ -0,0 +1,24 @@
+require "discorb"
+
+RSpec.describe Discorb::Button do
+  it "creates a select menu" do
+    expect {
+      Discorb::TextInput.new("text", "text", :short)
+    }.not_to raise_error
+  end
+  it "converts to payload" do
+    expect(Discorb::TextInput.new("text", "text", :short).to_hash).to eq(
+      {
+        custom_id: "text",
+        label: "text",
+        max_length: nil,
+        min_length: nil,
+        placeholder: nil,
+        required: false,
+        style: 1,
+        type: 4,
+        value: nil,
+      }
+    )
+  end
+end
