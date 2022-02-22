3da0f8f629d702a31a492fc67ebde2d97b2d576a
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2022-02-23 08:10:06 +0900

    Add: Enforce style in rubocop

---
+++ spec/components/component.spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+require "discorb"
+
+RSpec.describe Discorb::Component do
+  it "creates action row" do
+    expect(Discorb::Component.to_payload(
+      [Discorb::Button.new("label", :primary, custom_id: "id")]
+    )).to eq(
+      [
+        {
+          components: [
+            { 
+              custom_id: "id",
+              disabled: false,
+              emoji: nil,
+              label: "label",
+              style: 1,
+              type: 2, 
+            }
+          ],
+          type: 1,
+        }
+      ]
+    )
+  end
+end
