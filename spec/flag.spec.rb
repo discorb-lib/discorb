bbb250d1dd4cf1c67576d7c750f20409439fbc76
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2022-03-07 19:44:06 +0900

    Fix: Fix naming of test

--- spec/flag.spec.rb
+++ spec/flag.spec.rb
@@ -5,7 +5,7 @@ require "objspace"
 ObjectSpace.each_object(Class).filter { |c| c < Discorb::Flag }.each do |klass|
   RSpec.describe klass do
     klass.bits.each do |key, index|
-      specify "##{key} should be associated with 1 << #{index}" do
+      specify "##{key} is associated with 1 << #{index}" do
         expect(klass.new(1 << index).send(key)).to be true
       end
     end
