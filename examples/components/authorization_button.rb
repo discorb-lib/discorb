a543f5c172bdde93d03a1423e404f2e505a1b122
Nanashi <sevenc7c@sevenc7c.com>
2022-12-18 09:13:01 +0900

    Fix: Fix intents

--- examples/components/authorization_button.rb
+++ examples/components/authorization_button.rb
@@ -2,7 +2,7 @@
 
 require "discorb"
 
-client = Discorb::Client.new
+client = Discorb::Client.new(intents: Discorb::Intents.new(message_content: true))
 
 client.once :standby do
   puts "Logged in as #{client.user}"
