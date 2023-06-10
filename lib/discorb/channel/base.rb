1896dac308a1d0c01fd13a1f6482304e41761311
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-06-11 07:12:18 +0900

    Fix: Fix test fails

--- lib/discorb/channel/base.rb
+++ lib/discorb/channel/base.rb
@@ -64,7 +64,7 @@ module Discorb
     #
     def self.make_channel(client, data, no_cache: false)
       descendants.each do |klass|
-        if !klass.channel_type.nil? && klass.channel_type == data[:type]
+        if !klass.channel_type.nil? && klass.channel_type == data[:type] && klass != ForumChannel::Post
           return klass.new(client, data, no_cache: no_cache)
         end
       end
