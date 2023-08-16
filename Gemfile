f7efcc30bd6584ea7be2a324468451a1ce4949a4
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-08-16 10:23:46 +0900

    WIP: steep is not working well?

--- Gemfile
+++ Gemfile
@@ -5,32 +5,32 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in discorb.gemspec
 gemspec
 
-gem "rake", "~> 13.0"
+gem "rake", "~> 13.0", require: false
 
 # Rubocop
-gem "rubocop", "~> 1.25"
-gem "rubocop-rake", "~> 0.6.0"
-gem "rubocop-rspec", "~> 2.9"
+gem "rubocop", "~> 1.25", require: false
+gem "rubocop-rake", "~> 0.6.0", require: false
+gem "rubocop-rspec", "~> 2.9", require: false
 
 # RSpec
-gem "async-rspec", "~> 1.17"
-gem "parallel_tests", "~> 4.2"
-gem "rspec", "~> 3.12"
+gem "async-rspec", "~> 1.17", require: false
+gem "parallel_tests", "~> 4.2", require: false
+gem "rspec", "~> 3.12", require: false
 
 # Typecheck
-gem "steep", "~> 1.1"
+gem "steep", "~> 1.5", require: false
 
 # Other development tools
-gem "lefthook", "~> 1.4.3"
-gem "sord", "~> 3.0.1"
+gem "lefthook", "~> 1.4.3", require: false
+gem "sord", "~> 3.0.1", require: false
 
 group :docs, optional: true do
-  gem "crowdin-api", "~> 1.0"
-  gem "gettext", "~> 3.4.1"
-  gem "redcarpet"
-  gem "rubyzip", "~> 2.3"
-  gem "yard", "~> 0.9.26"
+  gem "crowdin-api", "~> 1.0", require: false
+  gem "gettext", "~> 3.4.1", require: false
+  gem "redcarpet", require: false
+  gem "rubyzip", "~> 2.3", require: false
+  gem "yard", "~> 0.9.26", require: false
 end
 
-gem "syntax_tree", "~> 2.8"
-gem "syntax_tree-rbs", "~> 0.5.0"
+gem "syntax_tree", "~> 2.8", require: false
+gem "syntax_tree-rbs", "~> 0.5.0", require: false
--- lib/discorb/gateway_requests.rb
+++ lib/discorb/gateway_requests.rb
@@ -5,6 +5,13 @@ module Discorb
   # Represents an activity for Gateway Command.
   #
   class Activity
+    # @return [String] The text of the activity.
+    attr_reader :text
+    # @return [:playing, :streaming, :listening, :watching, :custom, :competing] The type of the activity.
+    attr_reader :type
+    # @return [String] The URL of the activity.
+    attr_reader :url
+
     # @private
     # @return [{Symbol => Integer}] The mapping of activity types.
     TYPES = {
@@ -12,20 +19,27 @@ module Discorb
       streaming: 1,
       listening: 2,
       watching: 3,
+      custom: 4,
       competing: 5
     }.freeze
 
     #
     # Initializes a new Activity.
     #
-    # @param [String] name The name of the activity.
-    # @param [:playing, :streaming, :listening, :watching, :competing] type The type of activity.
+    # @param [String] text The text of the activity.
+    # @param [:playing, :streaming, :listening, :watching, :custom, :competing] type The type of activity.
     # @param [String] url The URL of the activity.
     #
-    def initialize(name, type = :playing, url = nil)
-      @name = name
-      @type = TYPES[type] or
-        raise ArgumentError, "Invalid activity type: #{type}"
+    def initialize(text, type = :playing, url: nil)
+      @text = text
+      @type =
+        (
+          if TYPES.key?(type)
+            TYPES[type]
+          else
+            raise(ArgumentError, "invalid activity type: #{type}")
+          end
+        )
       @url = url
     end
 
@@ -35,7 +49,11 @@ module Discorb
     # @return [Hash] A hash representation of the activity.
     #
     def to_hash
-      { name: @name, type: @type, url: @url }
+      if @type == :custom
+        { state: @text, type: @type, url: @url }
+      else
+        { name: @text, type: @type, url: @url }
+      end
     end
 
     def inspect
